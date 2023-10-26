ffded6b3d6c560d63c5005029c3b164c3d207242
Qi Zhao <zhaoqi99@outlook.com>
2023-10-26 16:53:15 +0800

    perf: ⚡️ rest_framework.viewsets

--- drf_cached_view/rest_framework/viewsets.py
+++ drf_cached_view/rest_framework/viewsets.py
@@ -1,12 +1,12 @@
 from rest_framework.viewsets import ModelViewSet as DRFModelViewSet
 from rest_framework.viewsets import ReadOnlyModelViewSet as DRFReadOnlyModelViewSet
-from drf_cached_view.cache import ViewCache
+
 from drf_cached_view.mixins import CachedViewMixin
 
 
 class ModelViewSet(CachedViewMixin, DRFModelViewSet):
-    cache_class = ViewCache
+    pass
 
 
 class ReadOnlyModelViewSet(CachedViewMixin, DRFReadOnlyModelViewSet):
-    cache_class = ViewCache
+    pass
